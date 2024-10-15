cec3633ebbf4e386a9d8b9c3e2a2b349a06d8d98
Scriptscrypt <tirumaladasa@gmail.com>
2024-10-16 00:53:40 +0530

    Feat: NextJS configs

---
+++ integrate-with-next-js/.next/server/pages/index.js
@@ -0,0 +1,91 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(function() {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./components/CreativeEditorSDKNoSSR.jsx":
+/*!***********************************************!*\
+  !*** ./components/CreativeEditorSDKNoSSR.jsx ***!
+  \***********************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var next_dynamic__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! next/dynamic */ \"next/dynamic\");\n/* harmony import */ var next_dynamic__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(next_dynamic__WEBPACK_IMPORTED_MODULE_0__);\n\nconst CreativeEditorSDKWithNoSSR = next_dynamic__WEBPACK_IMPORTED_MODULE_0___default()(() => __webpack_require__.e(/*! import() */ \"components_CreativeEditorSDK_jsx\").then(__webpack_require__.bind(__webpack_require__, /*! ./CreativeEditorSDK */ \"./components/CreativeEditorSDK.jsx\")), {\n  ssr: false,\n  loadableGenerated: {\n    webpack: () => [/*require.resolve*/(/*! ./CreativeEditorSDK */ \"./components/CreativeEditorSDK.jsx\")],\n    modules: [\"../components/CreativeEditorSDKNoSSR.jsx -> \" + './CreativeEditorSDK']\n  }\n});\n/* harmony default export */ __webpack_exports__[\"default\"] = (CreativeEditorSDKWithNoSSR);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9pbnRlZ3JhdGUtd2l0aC1uZXh0LWpzLy4vY29tcG9uZW50cy9DcmVhdGl2ZUVkaXRvclNES05vU1NSLmpzeD85YWU0Il0sIm5hbWVzIjpbIkNyZWF0aXZlRWRpdG9yU0RLV2l0aE5vU1NSIiwiZHluYW1pYyIsInNzciJdLCJtYXBwaW5ncyI6Ijs7O0FBQUE7QUFFQSxNQUFNQSwwQkFBMEIsR0FBR0MsbURBQU8sQ0FBQyxNQUFNLDhMQUFQLEVBQXNDO0FBQzlFQyxLQUFHLEVBQUUsS0FEeUU7QUFBQTtBQUFBLHdDQUF4QiwrREFBd0I7QUFBQSwrREFBeEIscUJBQXdCO0FBQUE7QUFBQSxDQUF0QyxDQUExQztBQUlBLCtEQUFlRiwwQkFBZiIsImZpbGUiOiIuL2NvbXBvbmVudHMvQ3JlYXRpdmVFZGl0b3JTREtOb1NTUi5qc3guanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgZHluYW1pYyBmcm9tICduZXh0L2R5bmFtaWMnXG5cbmNvbnN0IENyZWF0aXZlRWRpdG9yU0RLV2l0aE5vU1NSID0gZHluYW1pYygoKSA9PiBpbXBvcnQoJy4vQ3JlYXRpdmVFZGl0b3JTREsnKSwge1xuICBzc3I6IGZhbHNlXG59KVxuXG5leHBvcnQgZGVmYXVsdCBDcmVhdGl2ZUVkaXRvclNES1dpdGhOb1NTUjtcbiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./components/CreativeEditorSDKNoSSR.jsx\n");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": function() { return /* binding */ Home; }\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _components_CreativeEditorSDKNoSSR__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/CreativeEditorSDKNoSSR */ \"./components/CreativeEditorSDKNoSSR.jsx\");\n\nvar _jsxFileName = \"/Users/Crypt/Code/One Last Folder/Websites/cesdk-imgly/cesdk-web-examples/integrate-with-next-js/pages/index.js\";\n\nfunction Home() {\n  let config = {// baseURL: 'assets/'\n  };\n  return /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_components_CreativeEditorSDKNoSSR__WEBPACK_IMPORTED_MODULE_1__.default, {\n    config: config\n  }, void 0, false, {\n    fileName: _jsxFileName,\n    lineNumber: 7,\n    columnNumber: 10\n  }, this);\n}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9pbnRlZ3JhdGUtd2l0aC1uZXh0LWpzLy4vcGFnZXMvaW5kZXguanM/NDRkOCJdLCJuYW1lcyI6WyJIb21lIiwiY29uZmlnIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFBQTtBQUVlLFNBQVNBLElBQVQsR0FBZ0I7QUFDN0IsTUFBSUMsTUFBTSxHQUFHLENBQ1g7QUFEVyxHQUFiO0FBR0Esc0JBQU8sOERBQUMsdUVBQUQ7QUFBNEIsVUFBTSxFQUFFQTtBQUFwQztBQUFBO0FBQUE7QUFBQTtBQUFBLFVBQVA7QUFDRCIsImZpbGUiOiIuL3BhZ2VzL2luZGV4LmpzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IENyZWF0aXZlRWRpdG9yU0RLV2l0aE5vU1NSICBmcm9tICcuLi9jb21wb25lbnRzL0NyZWF0aXZlRWRpdG9yU0RLTm9TU1InXG5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIEhvbWUoKSB7XG4gIGxldCBjb25maWcgPSB7XG4gICAgLy8gYmFzZVVSTDogJ2Fzc2V0cy8nXG4gIH1cbiAgcmV0dXJuIDxDcmVhdGl2ZUVkaXRvclNES1dpdGhOb1NTUiBjb25maWc9e2NvbmZpZ30gLz5cbn1cbiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "@cesdk/cesdk-js":
+/*!**********************************!*\
+  !*** external "@cesdk/cesdk-js" ***!
+  \**********************************/
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("@cesdk/cesdk-js");;
+
+/***/ }),
+
+/***/ "next/dynamic":
+/*!*******************************!*\
+  !*** external "next/dynamic" ***!
+  \*******************************/
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dynamic");;
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react");;
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react/jsx-dev-runtime");;
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
